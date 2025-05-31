eedea2c29b854cf446a807e0f9300faa9023615c
Abhiram-s-kumar <abhiramskumar75@gmail.com>
2025-05-31 11:31:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import ItemSearch from './pages/ItemSearch';
+import { getSession } from './utils/session';
+
+
+function App() {
+  const session = getSession();
+
+  return (
+    <Routes>
+      <Route path="/item" element={session ? <Navigate to="/item-search" /> : <Navigate to="/login" />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/item-search" element={session ? <ItemSearch /> : <Navigate to="/login" />} />
+    </Routes>
+  );
+}
+
+export default App;
+
+
